a4224bf951c5faac91233173dbcd59d84b68b7ff
Avinash Karmjit <rk442537@gmail.com>
2024-08-27 03:47:51 +0530

    project setup

---
+++ src/main/java/com/example/bookstore_api/model/Admin.java
@@ -0,0 +1,25 @@
+package com.example.bookstore_api.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true, nullable = false)
+    private  String email;
+}
