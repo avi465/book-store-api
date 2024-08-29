c50429b47640f84d2ae900691c956b38cae4363d
Mayank Gopal <Mayank.Gopal@godigit.com>
2024-08-29 13:29:37 +0530

    dto

--- src/main/java/com/example/bookstore_api/model/User.java
+++ src/main/java/com/example/bookstore_api/model/User.java
@@ -15,7 +15,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true)
+    @Column(unique = true ,nullable= false)
     private String username;
 
     @Column(nullable = false)
